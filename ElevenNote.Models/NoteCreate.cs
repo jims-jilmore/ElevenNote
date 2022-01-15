a060c7cb2ba6a0d03642d0c44829fa626a5413d9
jims-jilmore <92383003+jims-jilmore@users.noreply.github.com>
2022-01-15 13:02:28 -0500

    Note Data ,Models, and Services Created

---
+++ ElevenNote.Models/NoteCreate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Models
+{
+    public class NoteCreate
+    {
+        [Required]
+        [MinLength(1, ErrorMessage = "Enter At Least 1 Character")]
+        [MaxLength(50, ErrorMessage = "Too Many Characters | Max 50")]
+        public string Title { get; set; }
+
+        [MaxLength(8000)]
+        public string Content { get; set; }
+    }
+}
